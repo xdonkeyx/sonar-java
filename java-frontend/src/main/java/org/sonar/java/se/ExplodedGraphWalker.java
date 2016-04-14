07feda40ce894f6a12770b6f65126e28cab35059
DidierBesset <didier.besset@sonarsource.com>
2016-04-14 11:53:07 +0200

    Reset checkers with stacked contexts in case of exception

--- java-frontend/src/main/java/org/sonar/java/se/ExplodedGraphWalker.java
+++ java-frontend/src/main/java/org/sonar/java/se/ExplodedGraphWalker.java
@@ -182,6 +182,7 @@ public class ExplodedGraphWalker extends BaseTreeVisitor {
     while (!workList.isEmpty()) {
       steps++;
       if (steps > MAX_STEPS) {
+        checkerDispatcher.executeExceptionEndOfExecution();
         throw new MaximumStepsReachedException("reached limit of " + MAX_STEPS + " steps for method " + tree.simpleName().name() + " in class " + tree.symbol().owner().name());
       }
       // LIFO:
@@ -210,6 +211,7 @@ public class ExplodedGraphWalker extends BaseTreeVisitor {
           handleBlockExit(programPosition);
         }
       } catch (ExplodedGraphWalker.TooManyNestedBooleanStatesException e) {
+        checkerDispatcher.executeExceptionEndOfExecution();
         throw new MaximumStepsReachedException(
           "reached maximum number of " + MAX_NESTED_BOOLEAN_STATES + " branched states for method " + tree.simpleName().name() + " in class " + tree.symbol().owner().name(), e);
       }
